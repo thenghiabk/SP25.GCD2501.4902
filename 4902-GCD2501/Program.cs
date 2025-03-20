2d92760b11344121785f8f1cc3febf64bbd25d47
thenghiabk <thenghiabk@gmail.com>
2025-03-20 11:02:36 +0700

    Add project files.

---
+++ 4902-GCD2501/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4902_GCD2501
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Example 1: Output 
+               See more: https://www.w3schools.com/cs/cs_output.php
+             */
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine(2025);
+            //Console.WriteLine("Year " + 2025);
+            //Console.WriteLine("Hello " + "World");
+            //Console.WriteLine(2025 + 1);
+            //Console.WriteLine(2025 - 1);
+            //Console.WriteLine("Next year is: " + (2025 + 1));
+            //Console.WriteLine($"Previous year is: {(2025 - 1)}");
+
+            /* 
+             * Example 2: Variables 
+             * See more: https://www.w3schools.com/cs/cs_variables.php
+             */
+
+            //int a = 5;
+            //int b = 0;
+            //b = 7;
+            //Console.WriteLine($"sum(a+b) = {(a+b)}");
+
+            //string name = "John";
+            //Console.WriteLine($"Hello {name}");
+            //int age = 25;
+            //Console.WriteLine($"You are {age} years old");
+            //Console.WriteLine($"You were born in {2025 - age}");
+
+            /* 
+             * Example 3: Input 
+             * See more: https://www.w3schools.com/cs/cs_output.php
+             *      https://www.w3schools.com/cs/cs_type_casting.php
+             */
+
+            //Console.WriteLine("Please enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Hello {name}");
+
+            //Console.WriteLine("Please enter your age: ");
+            //string ageString = Console.ReadLine();
+            //int age = int.Parse(ageString);
+
+            //Console.WriteLine($"You are {age} years old");
+            //Console.WriteLine($"You were born in {2025 - age}");
+
+            /*
+             * Example 4: Boolean
+             * See more: https://www.w3schools.com/cs/cs_booleans.php 
+             */
+
+            //int a = 50;
+            //int b = 7;
+            //Console.WriteLine($"a < b: {a < b}");
+            //Console.WriteLine($"a > b: {a > b}");
+
+            /*
+             * Example 5: If statements
+             * See more: https://www.w3schools.com/cs/cs_conditions_elseif.php
+             */
+
+            //Console.WriteLine("Please enter x: ");
+            //string xString = Console.ReadLine();
+            //double x = double.Parse(xString);
+
+            //Console.WriteLine("Please enter y: ");
+            //double y = double.Parse(Console.ReadLine());
+
+            //if (x > y)
+            //{
+            //    Console.WriteLine("x is greater than y");
+            //}
+            //else if (x < y)
+            //{
+            //    Console.WriteLine("x is less than y");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("x is equal to y");
+            //}
+
+            /*
+             * Example 6: Switch statements
+             * See more: https://www.w3schools.com/cs/cs_switch.php
+             */
+
+            //Console.WriteLine("Please enter a day of the week (1-7): ");
+            //int day = int.Parse(Console.ReadLine());
+
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid day");
+            //        break;
+            //}
+
+            /*
+             * Example 7: While Loops
+             * See more: https://www.w3schools.com/cs/cs_while_loop.php
+             *           https://www.w3schools.com/cs/cs_break.php
+             */
+
+            //while (true)
+            //{
+            //    // block of code
+            //    Console.WriteLine("Please enter your name: ");
+            //    string name = Console.ReadLine();
+            //    Console.WriteLine($"Hello {name}");
+
+            //    Console.WriteLine("Do you want to continue? (y/n)");
+            //    string answer = Console.ReadLine();
+            //    if (answer == "n")
+            //    {
+            //        break;
+            //    }
+
+            //}
+
+
+            /*
+             * Example 8: While Loops + Switch Statements
+             */
+
+            while (true)
+            {
+                Console.WriteLine("\nContact Management System");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Edit Contact");
+                Console.WriteLine("3. Remove Contact");
+                Console.WriteLine("4. Search Contact");
+                Console.WriteLine("\nPlease choose an option (1-4): ");
+
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("--Add Contact--");
+                        Console.WriteLine("Please enter your name: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Please enter your phone number: ");
+                        string phoneNumber = Console.ReadLine();
+
+                        Console.WriteLine($"Contact (Name: {name}, Phone Number: {phoneNumber}) was added.");
+                        break;
+                    case 2:
+                        Console.WriteLine("--Edit Contact--");
+                        break;
+                    case 3:
+                        Console.WriteLine("--Remove Contact--");
+                        break;
+                    case 4:
+                        Console.WriteLine("--Search Contact--");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+
+                Console.WriteLine("\nDo you want to continue? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ ContactManagementSystemBasic/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagementSystemBasic
+{
+    public class Program
+    {
+        // Maximum number of contacts we can store (you can change this)
+        static int maxContacts = 100;
+
+        // Our arrays to store contact information
+        static string[] names = new string[maxContacts];
+        static string[] phoneNumbers = new string[maxContacts];
+        static string[] emails = new string[maxContacts];
+
+        // Keep track of how many contacts we have
+        static int contactCount = 0;
+
+        public static void Main(string[] args)
+        {
+            // The main loop of our program
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("\nContact Management System");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display Contacts");
+                Console.WriteLine("3. Find a Contact");
+                Console.WriteLine("4. Remove a Contact");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddContact();
+                        break;
+                    case "2":
+                        DisplayContacts();
+                        break;
+                    case "3":
+                        FindContact();
+                        break;
+                    case "4":
+                        RemoveContact();
+                        break;
+                    case "5":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Try again.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+        static void AddContact()
+        {
+            if (contactCount < maxContacts)
+            {
+                Console.Write("Enter contact name: ");
+                names[contactCount] = Console.ReadLine();
+
+                Console.Write("Enter phone number: ");
+                phoneNumbers[contactCount] = Console.ReadLine();
+
+                Console.Write("Enter email address: ");
+                emails[contactCount] = Console.ReadLine();
+
+                contactCount++; // Important: Increment the count!
+
+                Console.WriteLine("Contact added!");
+            }
+            else
+            {
+                Console.WriteLine("Contact list is full!");
+            }
+        }
+        static void DisplayContacts()
+        {
+            if (contactCount == 0)
+            {
+                Console.WriteLine("No contacts to display.");
+                return;
+            }
+
+            Console.WriteLine("Contacts:");
+            for (int i = 0; i < contactCount; i++)
+            {
+                Console.WriteLine($"  Name: {names[i]}, Phone: {phoneNumbers[i]}, Email: {emails[i]}");
+            }
+        }
+
+        static void FindContact()
+        {
+            if (contactCount == 0)
+            {
+                Console.WriteLine("No contacts to find.");
+                return;
+            }
+
+            Console.Write("Enter the name to search for: ");
+            string searchName = Console.ReadLine();
+
+            bool found = false;
+            for (int i = 0; i < contactCount; i++)
+            {
+                //Use .Equals and ignore cases when comparing
+                if (names[i].Equals(searchName, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"  Name: {names[i]}, Phone: {phoneNumbers[i]}, Email: {emails[i]}");
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("Contact Not Found.");
+            }
+        }
+
+        static void RemoveContact()
+        {
+            if (contactCount == 0)
+            {
+                Console.WriteLine("No contacts to remove.");
+                return;
+            }
+
+            Console.Write("Enter the name of the contact to remove: ");
+            string nameToRemove = Console.ReadLine();
+
+            int indexToRemove = -1; // -1 means we haven't found it yet
+
+            for (int i = 0; i < contactCount; i++)
+            {
+                if (names[i].Equals(nameToRemove, StringComparison.OrdinalIgnoreCase))
+                {
+                    indexToRemove = i;
+                    break; // Exit the loop once we find it
+                }
+            }
+
+            if (indexToRemove != -1)
+            {
+                // Shift elements to fill the gap
+                for (int i = indexToRemove; i < contactCount - 1; i++)
+                {
+                    names[i] = names[i + 1];
+                    phoneNumbers[i] = phoneNumbers[i + 1];
+                    emails[i] = emails[i + 1];
+                }
+
+                // "Clear" the last element (not strictly necessary, but good practice)
+                names[contactCount - 1] = null;
+                phoneNumbers[contactCount - 1] = null;
+                emails[contactCount - 1] = null;
+
+                contactCount--; // Decrement the count
+
+                Console.WriteLine("Contact removed.");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+    }
+}
